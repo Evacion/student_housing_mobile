906eb0b556d3b0dee48cc5a2828734ea9fe97624
Evacion <158044203+Evacion@users.noreply.github.com>
2024-04-18 09:51:01 +0800

    v2
    Updated Notepad

--- notepad.dart
+++ notepad.dart
@@ -2,9 +2,19 @@ bool isPetsAllowed = true;
 bool isVisitorsAllowed = true;
 bool isVisible = true;
 String name = "";
-String location = "";
 String description = "";
 int pricing = 0;
+const location = {
+  "lat": int,
+  "long": int,
+  "zipcode": int,
+  "city": String,
+  "province": String,
+};
+const housePhotoUrl = [
+  "insert URL here",
+  "insert URL here",
+];
 const contact = {
 	"number": int,
 	"email": String,
@@ -16,51 +26,49 @@ const rooms = {
 		"hasOwnBathroom": bool,
 		"maxCapacity": int,
 		"pricing": int,
+    "roomPhotoUrl": [
+      String,
+      String,
+    ],
+    "ammenities": [
+      "Twin sized Bed",
+      "Aircondition",
+      "Mini-Fridge",
+      "Free Wi-Fi",
+      "Water Bill Provided",
+      "Bathroom exists",
+    ],
+	},
+	1: {
+		"isVacant": bool,
+		"hasAirconditioning": bool,
+		"hasOwnBathroom": bool,
+		"maxCapacity": int,
+		"pricing": int,
+    "roomPhotoUrl": [ 
+      String, 
+      String,
+    ],
+    "ammenities": [
+      "Twin sized Bed",
+      "Aircondition",
+      "Mini-Fridge",
+      "Free Wi-Fi",
+      "Water Bill Provided",
+      "Bathroom exists",
+    ],
 	},
 };
 
 
-// String agreementType = "Owner Agreemtn";
-// int bathrooms = 1;
-// String bills = "";
-// const contacts = {
-//   "number": 1231203,
-//   "email": "amogusdssw",
-// };
-// String description = "Amogus SKibbidi Ohio Toilet";
-// const geolocation = ["longitude", "latitude"];
-// bool isAirconditioning = true;
-// bool isCouples = false;
-// bool isDisablityAccess = true;
-// bool isGayFriendly = true;
-// bool isLeaseNegotiable = true;
-// bool isParking = false;
-// bool isPetsAllowed = false;
-// bool isSharehouse = true;
-// bool isVisible = true;
-// String lease = "1-36 months";
-// String lister = "John Filipinoman";
-// String listingName = "Ponce Dormitories";
-// String listingLocation = "Ponce Street, Davao City";
-// const occupancy = {
-//   "occupants": 1,
-//   "vacancies": 2,
-// };
-// String parkingLoc = "N/A";
-// int pricing = 500;
-// String transit = "Tricycle";
-// String safety = "Working Smoke Detector, Security Guard at front, Individual Keys for each Room";
-// String smoking = "OutsideOnly";
-// const rooms = {
-//   0: {
-//     "availability": "dateTime",
-//     "description": "Desc",
-//     "pricing": "P300 / week"
-//   },
-//   1: {
-//     "availability": "dateTime",
-//     "description": "Desc",
-//     "pricing": "P300 / week"
-//   },
-// };
+//we can probably change descriptions to an array called ammenities, for example:
 
+
+const ammenities = [
+  "Twin sized Bed",
+  "Aircondition",
+  "Mini-Fridge",
+  "Free Wi-Fi",
+  "Water Bill Provided",
+  "Bathroom exists",
+];
