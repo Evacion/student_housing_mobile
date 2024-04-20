e483c7821d744b9022ee104ae1d5922f53812a3e
Ray James Gonzales <54382521+EvacionSaraak@users.noreply.github.com>
2024-04-21 00:36:54 +0800

    Revert "Updated notepad"
    This reverts commit ee1465d960dd88f075d551f4825fbaea99c554e4.

--- notepad.dart
+++ notepad.dart
@@ -1,66 +1,43 @@
-bool isPetsAllowed = true;
-bool isVisitorsAllowed = true;
+String agreementType = "Owner Agreemtn";
+int bathrooms = 1;
+String bills = "";
+const contacts = {
+  "number": 1231203,
+  "email": "amogusdssw",
+};
+String description = "Amogus SKibbidi Ohio Toilet";
+const geolocation = ["longitude", "latitude"];
+bool isAirconditioning = true;
+bool isCouples = false;
+bool isDisablityAccess = true;
+bool isGayFriendly = true;
+bool isLeaseNegotiable = true;
+bool isParking = false;
+bool isPetsAllowed = false;
+bool isSharehouse = true;
 bool isVisible = true;
-String name = "";
-String location = "";
-String description = "";
-int pricing = 0;
-const contact = {
-	"number": int,
-	"email": String,
+String lease = "1-36 months";
+String lister = "John Filipinoman";
+String listingName = "Ponce Dormitories";
+String listingLocation = "Ponce Street, Davao City";
+const occupancy = {
+  "occupants": 1,
+  "vacancies": 2,
 };
+String parkingLoc = "N/A";
+int pricing = 500;
+String transit = "Tricycle";
+String safety = "Working Smoke Detector, Security Guard at front, Individual Keys for each Room";
+String smoking = "OutsideOnly";
 const rooms = {
-	0: {
-		"isVacant": bool,
-		"hasAirconditioning": bool,
-		"hasOwnBathroom": bool,
-		"maxCapacity": int,
-		"pricing": int,
-	},
+  0: {
+    "availability": "dateTime",
+    "description": "Desc",
+    "pricing": "P300 / week"
+  },
+  1: {
+    "availability": "dateTime",
+    "description": "Desc",
+    "pricing": "P300 / week"
+  },
 };
-
-
-// String agreementType = "Owner Agreemtn";
-// int bathrooms = 1;
-// String bills = "";
-// const contacts = {
-//   "number": 1231203,
-//   "email": "amogusdssw",
-// };
-// String description = "Amogus SKibbidi Ohio Toilet";
-// const geolocation = ["longitude", "latitude"];
-// bool isAirconditioning = true;
-// bool isCouples = false;
-// bool isDisablityAccess = true;
-// bool isGayFriendly = true;
-// bool isLeaseNegotiable = true;
-// bool isParking = false;
-// bool isPetsAllowed = false;
-// bool isSharehouse = true;
-// bool isVisible = true;
-// String lease = "1-36 months";
-// String lister = "John Filipinoman";
-// String listingName = "Ponce Dormitories";
-// String listingLocation = "Ponce Street, Davao City";
-// const occupancy = {
-//   "occupants": 1,
-//   "vacancies": 2,
-// };
-// String parkingLoc = "N/A";
-// int pricing = 500;
-// String transit = "Tricycle";
-// String safety = "Working Smoke Detector, Security Guard at front, Individual Keys for each Room";
-// String smoking = "OutsideOnly";
-// const rooms = {
-//   0: {
-//     "availability": "dateTime",
-//     "description": "Desc",
-//     "pricing": "P300 / week"
-//   },
-//   1: {
-//     "availability": "dateTime",
-//     "description": "Desc",
-//     "pricing": "P300 / week"
-//   },
-// };
-
