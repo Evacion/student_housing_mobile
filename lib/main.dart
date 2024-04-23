42701f53d6d6b420626645d1f3e12ad6af549c62
Evacion <158044203+Evacion@users.noreply.github.com>
2024-04-23 09:50:00 +0800

    Things were performed upon my rectum
    THAT'S RIGHT I DON'T USE THIS FEATURE CORRECTLY. WHAT'RE YOU GONNA DO ABOUT IT!?

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,38 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:student_housing_mobile/views/home.dart';
+import 'package:student_housing_mobile/views/home.dart'; 
 import 'package:student_housing_mobile/views/login.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  final initializationFuture = Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyB2ye5vscFm1eiOiPVBIQ8np-CnGNk4DQc",
+      appId: "1:997141921548:web:70ec9011eff4595be8d329",
+      messagingSenderId: "997141921548",
+      projectId: "student-housing-db",
+      authDomain: "student-housing-db.firebaseapp.com",
+      storageBucket: "student-housing-db.appspot.com",
+      measurementId: "G-Y85GR73R4B",
+      androidClientId: "997141921548-vh53d2d1dcckitdhocgjord2dl0jkbuu.apps.googleusercontent.com",
+      iosClientId: "997141921548-mvfia8o80frr23gkfnr9atclk4r5i67q.apps.googleusercontent.com",
+    ),
+  );
 
-  runApp(const MyApp());
+  // Future completion checker
+  initializationFuture.then((_) {
+    if (kDebugMode) {
+      runApp(const MyApp());
+    }
+  }).catchError((error) {
+    if (kDebugMode) {
+      print('Firebase initialization error: $error');
+    }
+  });
+
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
